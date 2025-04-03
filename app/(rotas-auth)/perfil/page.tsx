30fcb448b100a473b76cab0108433c1c0ae1a6a2
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2025-04-03 14:17:50 -0300

    style: adicionar espaçamento inferior ao componente de perfil para melhor layout

--- app/(rotas-auth)/perfil/page.tsx
+++ app/(rotas-auth)/perfil/page.tsx
@@ -32,7 +32,7 @@ export default async function Perfil() {
 	const userData = user as Partial<IUsuario>;
 
 	return (
-		<div className='mx-auto px-0 md:px-8 w-full'>
+		<div className='mx-auto px-0 md:px-8 pb-10 w-full'>
 			<h1 className='font-bold text-4xl mt-5'>Perfil</h1>
 
 			<div className='grid grid-cols-1 md:grid-cols-3 gap-8 my-5'>
